460a5ffadb1e274e7b376839b99a31c8a0f22315
colvin <39858617+colvin@users.noreply.github.com>
2019-01-06 16:03:08 -0500

    build man-pages

--- mk/build.mk
+++ mk/build.mk
@@ -68,7 +68,8 @@ BOOTSTRAP_TWO = \
 	xz
 
 SYSTEM_ONE = \
-	linux
+	linux \
+	man-pages
 
 SYSTEM_TWO =
 
@@ -222,12 +223,12 @@ chroot-build: chroot-check
 	@echo
 	for pkg in $(SYSTEM_ONE); do \
 		echo ; echo "====> $$pkg" ; echo ; \
-		$(MAKE) -C $(PKG_DIR)/$$pkg build clean || exit 1 ;\
+		$(MAKE) -C $(PKG_DIR)/$$pkg build install clean || exit 1 ;\
 	done
 	## TODO: adjust toolchain
 	for pkg in $(SYSTEM_TWO); do \
 		echo ; echo "====> $$pkg" ; echo ; \
-		$(MAKE) -C $(PKG_DIR)/$$pkg build clean || exit 1 ;\
+		$(MAKE) -C $(PKG_DIR)/$$pkg build install clean || exit 1 ;\
 	done
 
 build-cleanup:
